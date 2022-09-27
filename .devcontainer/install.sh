f4ae00d2839e4797059e4dd701f83fb9477b14c7
cgduynguyen <cgduy.nguyen@gmail.com>
2022-09-27 07:17:49 +0000

    Use ubuntu with node

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,16 +1,16 @@
-set
+set -e
 
-sudo apt -y install neovim xsel wl-clipboard ripgrep moreutils
+sudo apt -y install moreutils
 
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-git clone https://github.com/LunarVim/nvim-basic-ide.git ~/.config/nvim
-
 cp .devcontainer/.zshrc $HOME/.zshrc
 cp .devcontainer/.p10k.zsh $HOME/.p10k.zsh
 
-pip install pynvim
+sudo apt -y install neovim xsel wl-clipboard ripgrep
+
 npm i -g neovim
 
+git clone https://github.com/LunarVim/nvim-basic-ide.git $HOME/.config/nvim
