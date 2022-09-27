2322a5a6fe1925c8d2c700036e252e2f743b5244
cgduynguyen <cgduy.nguyen@gmail.com>
2022-09-27 05:47:04 +0000

    Add neovim

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,10 +1,16 @@
 set
 
-sudo apt -y install moreutils
+sudo apt -y install neovim xsel wl-clipboard ripgrep moreutils
 
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+git clone https://github.com/LunarVim/nvim-basic-ide.git ~/.config/nvim
+
 cp .devcontainer/.zshrc $HOME/.zshrc
-cp .devcontainer/.p10k.zsh $HOME/.p10k.zsh
+cp .devcontainer/.p10k.zsh $HOME/.p10k.zshi
+
+pip install pynvim
+npm i -g neovim
+
